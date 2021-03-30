41a310008e02f28457c391b1376a6828b41e0f45
Gabriel oguna <gabeoguna@gmail.com>
2021-03-30 17:31:20 +0300

    Edited App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit Hii App Uandike kitu alafu Fuata Link hapo juu <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
